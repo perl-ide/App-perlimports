896b05c7c880e203f7a19e1700ad2c8185b0b466
Olaf Alders <olaf@wundersolutions.com>
2021-01-18 16:08:55 -0500

    Ignore Data::Printer

--- lib/App/perlimports.pm
+++ lib/App/perlimports.pm
@@ -368,6 +368,7 @@ sub _build_is_ignored {
     return 1 if $self->_include->version;
 
     my %ignore = (
+        'Data::Printer'          => 1,
         'Devel::Confess'         => 1,
         'Exception::Class'       => 1,
         'Exporter'               => 1,
