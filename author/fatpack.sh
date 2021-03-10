09f85cb231d0f984ff46d65b36e7eb2c987c96cc
Olaf Alders <olaf@wundersolutions.com>
2021-03-10 12:46:30 -0500

    perlimports is now a fatpacked script

---
+++ author/fatpack.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# This only needs to be run if author/script/perlimports has changed or if new
+# changes to the PPI fork have been pulled in.
+
+set -eux
+
+ROOT_DIR=$PWD
+
+cd inc/PPI || exit
+dzil build --in "$ROOT_DIR/.ppi_build"
+cd "$ROOT_DIR/inc" || exit
+fatpack-simple --dir "$ROOT_DIR/.ppi_build/lib" -o "$ROOT_DIR/script/perlimports" "$ROOT_DIR/author/script/perlimports"
