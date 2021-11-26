ca9b2cadebdcfb2c73cf9d8e097c9d5240f4c569
Olaf Alders <olaf@wundersolutions.com>
2021-11-26 16:45:35 -0500

    increment $VERSION after 0.000026 release

--- lib/App/perlimports.pm
+++ lib/App/perlimports.pm
@@ -3,7 +3,7 @@ package App::perlimports;
 use strict;
 use warnings;
 
-our $VERSION = '0.000026';
+our $VERSION = '0.000027';
 
 1;
 
--- lib/App/perlimports/ExportInspector.pm
+++ lib/App/perlimports/ExportInspector.pm
@@ -4,7 +4,7 @@ use Moo;
 
 ## no critic (TestingAndDebugging::ProhibitNoStrict)
 
-our $VERSION = '0.000026';
+our $VERSION = '0.000027';
 
 use App::perlimports::Sandbox ();
 use Class::Inspector          ();
