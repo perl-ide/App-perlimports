dbef248bf5c395bc3effbf6457873deabbea4409
Olaf Alders <olaf@wundersolutions.com>
2026-08-06 20:50:21 +0000

    increment $VERSION after 0.000061 release

--- lib/App/perlimports/Annotations.pm
+++ lib/App/perlimports/Annotations.pm
@@ -4,7 +4,7 @@ package App::perlimports::Annotations;
 
 use Moo;
 
-our $VERSION = '0.000061';
+our $VERSION = '0.000062';
 
 use Types::Standard qw( ArrayRef Object );
 
--- lib/App/perlimports/Config.pm
+++ lib/App/perlimports/Config.pm
@@ -3,7 +3,7 @@ package App::perlimports::Config;
 use Moo;
 use MooX::StrictConstructor;
 
-our $VERSION = '0.000061';
+our $VERSION = '0.000062';
 
 use List::Util             qw( uniq );
 use Path::Tiny             qw( path );
